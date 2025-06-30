afe949961ff428d14cda8eb7564851b778f3a43f
GlowPixels <shumilina0702@yandex.ru>
2025-06-30 16:28:58 +0300

    Add img from Projects

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./styles/main.css";
 import Navbar from "./Components/navbar/Navbar";
 import Footer from "./Components/footer/Footer";
 import Home from "./pages/Hom";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
         
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <Projects />
 
      
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,11 @@
-   const Projects = () => {
+import project01 from './../styles/img/projects/01.jpg';
+import project02 from './../styles/img/projects/02.jpg';
+import project03 from './../styles/img/projects/03.jpg';
+import project04 from './../styles/img/projects/04.jpg';
+import project05 from './../styles/img/projects/05.jpg';
+import project06 from './../styles/img/projects/06.jpg';  
+   
+const Projects = () => {
     return ( 
         <main className="section">
             <div className="container">
@@ -6,33 +13,33 @@
                 <ul className="projects">
                     <li className="project">
                         <a href="./project-page.html">
-                            <img src="./img/projects/01.jpg" alt="Project img" className="project__img" />
+                            <img src={project01} alt="Project img" className="project__img" />
                             <h3 className="project__title">Gaming streaming portal</h3>
                         </a>
                     </li>
                     <li className="project">
                         <a href="./project-page.html">
-                            <img src="./img/projects/02.jpg" alt="Project img" className="project__img" />
+                            <img src={project02} alt="Project img" className="project__img" />
                             <h3 className="project__title">Video service</h3>
                         </a>
                     </li>
                     <li className="project">
                         <a href="./project-page.html">
-                            <img src="./img/projects/03.jpg" alt="Project img" className="project__img" />
+                        <img src={project03} alt="Project img" className="project__img" />
                             <h3 className="project__title">Video portal</h3>
                         </a>
                     </li>
 
                     <li className="project">
-                        <img src="./img/projects/04.jpg" alt="Project img" className="project__img" />
+                        <img src={project04} alt="Project img" className="project__img" />
                         <h3 className="project__title">Dating app</h3>
                     </li>
                     <li className="project">
-                        <img src="./img/projects/05.jpg" alt="Project img" className="project__img" />
+                        <img src={project05} alt="Project img" className="project__img" />
                         <h3 className="project__title">Landing</h3>
                     </li>
                     <li className="project">
-                        <img src="./img/projects/06.jpg" alt="Project img" className="project__img" />
+                        <img src={project06} alt="Project img" className="project__img" />
                         <h3 className="project__title">Gaming community</h3>
                     </li>
 
@@ -42,5 +49,5 @@
      );
    }
     
-   export default Projects; 
+export default Projects; 
     
