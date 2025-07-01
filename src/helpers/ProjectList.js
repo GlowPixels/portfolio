d9df7920ca6e4d378f28916250950c1118157051
GlowPixels <shumilina0702@yandex.ru>
2025-07-01 14:55:38 +0300

    Uploading all projects

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,63 @@
+import project01 from "./../styles/img/projects/01.jpg";
+import project01Big from "./../styles/img/projects/01-big.jpg";
+
+import project02 from "./../styles/img/projects/02.jpg";
+import project02Big from "./../styles/img/projects/02-big.jpg";
+
+import project03 from "./../styles/img/projects/03.jpg";
+import project03Big from "./../styles/img/projects/03-big.jpg";
+
+import project04 from "./../styles/img/projects/04.jpg";
+import project04Big from "./../styles/img/projects/04-big.jpg";
+
+import project05 from "./../styles/img/projects/05.jpg";
+import project05Big from "./../styles/img/projects/05-big.jpg";
+
+import project06 from "./../styles/img/projects/06.jpg";
+import project06Big from "./../styles/img/projects/06-big.jpg";
+
+
+const projects = [
+    {
+        title: 'Gaming streaming portal',
+        skills: 'React, Node.js, MongoDB',
+        img: project01,
+        imgBig: project01Big,
+        gitHubLink: 'https://github.com',
+    },
+    {
+        title: 'Video service',
+        skills: 'React, PHP, MySql',
+        img: project02,
+        imgBig: project02Big,
+        gitHubLink: 'https://github.com',
+    },
+    {
+        title: 'Video service',
+        skills: 'React, Node.js, MongoDB',
+        img: project03,
+        imgBig: project03Big,
+        gitHubLink: 'https://github.com',
+    },
+    {
+        title: 'Dating app',
+        skills: 'React, Node.js, MongoDB',
+        img: project04,
+        imgBig: project04Big,
+        gitHubLink: 'https://github.com',
+    },
+    {
+        title: 'Landing',
+        skills: 'React, Node.js, MongoDB',
+        img: project05,
+        imgBig: project05Big,
+        gitHubLink: 'https://github.com',
+    },
+    {
+        title: 'Gaming community',
+        skills: 'React, Node.js, MongoDB',
+        img: project06,
+        imgBig: project06Big,
+        gitHubLink: 'https://github.com',
+    }
+]
