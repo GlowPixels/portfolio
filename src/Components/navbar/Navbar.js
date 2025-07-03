2c96d0a6f01f92f5c00b243c14f5a31353b30af6
GlowPixels <shumilina0702@yandex.ru>
2025-07-03 14:25:22 +0300

    Add isActive

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -6,6 +6,11 @@ import "./style.css";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+    const activeLink = 'nav-list__link nav-list__link--active';
+    const normalLink = 'nav-list__link';
+
+
     return ( 
         <nav className="nav">
            <div className="container">
@@ -22,17 +27,17 @@ const Navbar = () => {
 
                 <ul className="nav-list">
                     <li className="nav-list__item">
-                        <NavLink to="/" className="nav-list__link">
+                        <NavLink to="/" className={({isActive}) => isActive ? activeLink : normalLink}>
                             Home
                         </NavLink>
                     </li>
                     <li className="nav-list__item">
-                       <NavLink to="/projects" className="nav-list__link">
+                       <NavLink to="/projects" className={({isActive}) => isActive ? activeLink : normalLink}>
                             Projects
                         </NavLink>
                     </li>
                     <li className="nav-list__item">
-                        <NavLink to="/contacts" className="nav-list__link">
+                        <NavLink to="/contacts" className={({isActive}) => isActive ? activeLink : normalLink}>
                             Contacts
                         </NavLink>
                     </li>
