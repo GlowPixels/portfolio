3f6662d701c349ea93fa76c94b6a248cc1815572
GlowPixels <shumilina0702@yandex.ru>
2025-07-04 15:13:38 +0300

    If the system settings change, change the theme.

--- src/Components/btnDarkMode/BtnDarkMode.js
+++ src/Components/btnDarkMode/BtnDarkMode.js
@@ -1,14 +1,18 @@
 import { useEffect, useRef } from 'react';
 import { useLocalStorage } from '../../utils/useLocalStorage';
+import detectDarkMode from '../../utils/detectDarkMode';
 import sun from "./sun.svg";
 import moon from "./moon.svg";
 import "./style.css";
 
 
+
+
 const BtnDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage('darkMode', 'light')
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode());
 
     const btnRef = useRef(null);
+    
 
     useEffect(() => {
         if (darkMode === 'dark') {
@@ -20,6 +24,15 @@ const BtnDarkMode = () => {
         }
 
     }, [darkMode]);
+
+    useEffect(() => {
+        window
+            .matchMedia("(prefers-color-scheme: dark)")
+            .addEventListener("change", (event) => {
+                const newColorScheme = event.matches ? "dark" : "light";
+                setDarkMode(newColorScheme)
+            });
+    }, [])
 
     const toggleDarkMode = () => { 
        setDarkMode((currentValue) => {
